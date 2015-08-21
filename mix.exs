19ae1ef64cf88b5aa6b002cb6455094b878d7eec
Kash Nouroozi <knouroozi@gmail.com>
2015-08-21 12:53:40 -0400

    Let's publish this thing :shipit:

--- mix.exs
+++ mix.exs
@@ -4,12 +4,20 @@ defmodule Wizardry.Mixfile do
   def project do
     [app: :wizardry,
      description: "Simple, low-level user account framework for Phoenix Framework",
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.0",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
+
+  def package do
+    [licenses: ["MIT"],
+     contributors: ["Kash Nouroozi"],
+     links: %{"Github" => "https://github.com/knrz/wizardry"}]
+  end
+
 
   def application do
     [applications: [:logger, :comeonin]]
