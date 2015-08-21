10ae325b83229149552817d70507e79beb41e898
Kash Nouroozi <knouroozi@gmail.com>
2015-08-21 13:24:27 -0400

    authenticate?

--- lib/wizardry.ex
+++ lib/wizardry.ex
@@ -3,25 +3,27 @@ defmodule Wizardry do
 
   alias Wizardry.Config
 
-  def init(options), do: options
-
-  def call(conn, options) do
-    conn = fetch_session(conn)
-
-    conn
-    |> assign_param(Config.session_key)
-    |> authenticate(Config.session_key, options[:authenticate])
+  def init(options) do
+    options
   end
 
-  defp assign_param(conn, param) do
+  def call(conn, options) do
+    authenticate? = Keyword.get(options, :authenticate, false)
+
+    fetch_session(conn)
+    |> assign_param(Config.session_key)
+    |> authenticate(Config.session_key, authenticate?)
+  end
+
+  def assign_param(conn, param) do
     case get_session(conn, param) do
       nil   -> conn
       value -> assign(conn, param, value)
     end
   end
 
-  defp authenticate(conn, _, false), do: conn
-  defp authenticate(conn, param, true) do
+  def authenticate(conn, _, false), do: conn
+  def authenticate(conn, param, true) do
     if get_session(conn, param), do: conn, else: halt(conn)
   end
 end
